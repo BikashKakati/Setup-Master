8e61f4c000b62e5f5faf121cecc59390b7a50103
swapnilchougule00 <swapnilchougule00@gmail.com>
2024-10-12 15:28:27 +0530

    Add repository URL to package.json

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,43 +1,86 @@
 import { DependencyOrCategory } from "../types";
 
 export const dependencies: DependencyOrCategory[] = [
-    {
-      label: "Frontend",
-      collapsible: true,
-      children: [
-        {
-          label: "Languages",
-          collapsible: true,
-          children: [
-            { label: "JavaScript", value: "js", checked: false, icon: "javascript" },
-            { label: "TypeScript", value: "ts", checked: false, icon: "typescript" },
-          ]
-        },
-        {
-          label: "Frameworks",
-          collapsible: true,
-          children: [
-            { label: "React", value: "react", checked: false, icon: "react" },
-            { label: "Next", value: "next", checked: false, icon: "next" },
-            { label: "Vite", value: "vite", checked: false, icon: "vite" },
-          ]
-        },
-        {
-          label: "Styles",
-          collapsible: true,
-          children: [
-            { label: "Tailwind CSS", value: "tailwind", checked: false, icon: "tailwind-icon" },
-            { label: "Bootstrap", value: "bootstrap", checked: false, icon: "bootstrap-icon" },
-          ]
-        },
-        {
-          label: "Other Libraries",
-          collapsible: true,
-          children: [
-            { label: "Axios", value: "axios", checked: false, icon: "axios-icon" },
-            { label: "Redux", value: "redux", checked: false, icon: "redux-icon" },
-          ]
-        },
-      ],
-    },
-  ];
+  {
+    label: "Frontend",
+    collapsible: true,
+    children: [
+      {
+        label: "Languages",
+        collapsible: true,
+        children: [
+          {
+            label: "JavaScript",
+            value: "js",
+            checked: false,
+            icon: "javascript",
+          },
+          {
+            label: "TypeScript",
+            value: "ts",
+            checked: false,
+            icon: "typescript",
+          },
+        ],
+      },
+      {
+        label: "Frameworks",
+        collapsible: true,
+        children: [
+          { label: "React", value: "react", checked: false, icon: "react" },
+          { label: "Next", value: "next", checked: false, icon: "next" },
+          { label: "Vite", value: "vite", checked: false, icon: "vite" },
+        ],
+      },
+      {
+        label: "Styles",
+        collapsible: true,
+        children: [
+          {
+            label: "Tailwind CSS",
+            value: "tailwind",
+            checked: false,
+            icon: "tailwind-icon",
+          },
+          {
+            label: "Shadcn",
+            value: "shdcn",
+            checked: false,
+            icon: "shadcn-icon",
+          },
+          {
+            label: "Bootstrap",
+            value: "bootstrap",
+            checked: false,
+            icon: "bootstrap-icon",
+          },
+        ],
+      },
+      {
+        label: "Other Libraries",
+        collapsible: true,
+        children: [
+          {
+            label: "Axios",
+            value: "axios",
+            checked: false,
+            icon: "axios-icon",
+          },
+          {
+            label: "Redux",
+            value: "redux",
+            checked: false,
+            icon: "redux-icon",
+          },
+          { label: "Zod", value: "zod", checked: false, icon: "zod-icon" },
+          {
+            label: "React Hook Form",
+            value: "reactHookForm",
+            checked: false,
+            icon: "reactHookForm-icon",
+          },
+        ],
+      },
+    ],
+  },
+];
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+﻿
+      module.exports = {
+        content: ['./src/**/*.{html,js,ts,tsx}'],
+        theme: {
+          extend: {},
+        },
+        plugins: [],
+      };
