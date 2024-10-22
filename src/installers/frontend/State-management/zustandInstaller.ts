27f725298a0c6b492aaf8630c45d7c8bd85009e3
Swapnil Chouhule <102139241+swapnilchougule00@users.noreply.github.com>
2024-10-22 10:31:56 +0530

    FIx : typo in zustandInstaller

--- src/installers/frontend/State-management/zustandInstaller.ts
+++ src/installers/frontend/State-management/zustandInstaller.ts
@@ -3,7 +3,7 @@ import * as vscode from "vscode";
 
 export class zustandInstaller extends DependencyInstaller {
   install() {
-    this.runCommand("npm install zustnd");
+    this.runCommand("npm install zustand");
     vscode.window.showInformationMessage("Zustand installed successfully!");
   }
 }
